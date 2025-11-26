d56beae5ab8bf14fc659b053e7bd808b0df2eab3
josimeiredantas18-star <josimeire.dantas18@gmail.com>
2025-11-26 20:16:32 -0300

    atualizando

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,30 @@
-alert('Oi fui carregar');
+// Vamos criar os elementos
+// do HTML no javascript 
+//dinamicamicamente
+
+function criarItemCardapio() {
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'itemCardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao 
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto 
+    img.className = 'img-item'
+
+    //Adicionando os elementos na div Mãe
+    const divC = document.getElementById('cardapio')
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+
+//executando a função
+criarItemCardapio('Bolo de Chocolate', 'Um clássico irresistível com camadas de chocolate', 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
