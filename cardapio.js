bdb5a99f442736e60fa57f7585f7e1f609e44c51
josimeiredantas18-star <josimeire.dantas18@gmail.com>
2025-12-05 19:24:10 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -1,35 +1,51 @@
-// Vamos criar os elementos
-// do HTML no javascript 
-//dinamicamicamente
+//Vamos criar os elementos
+//do html om javascript
+//dinamicamente
 
 function criarItemCardapio(titulo, descricao, foto) {
-    const divItemCardapio = document.createElement('div')
-    divItemCardapio.className = 'itemCardapio'
+    const divItemCArdapio = document.createElement('div')
+    divItemCArdapio.className = 'item-carpadio'
 
     const h3Titulo = document.createElement('h3')
     h3Titulo.textContent = titulo
 
     const pDescricao = document.createElement('p')
-    pDescricao.textContent = descricao 
+    pDescricao.textContent = descricao
     pDescricao.className = 'descricao'
 
     const img = document.createElement('img')
-    img.src = foto 
+    img.src = foto
     img.className = 'img-item'
 
-    //Adicionando os elementos na div Mãe
+    //adicionando os elementos na div mãe
     const divC = document.getElementById('cardapio')
 
-    divItemCardapio.appendChild(h3Titulo)
-    divItemCardapio.appendChild(pDescricao)
-    divItemCardapio.appendChild(img)
+    divItemCArdapio.appendChild(h3Titulo)
+    divItemCArdapio.appendChild(pDescricao)
+    divItemCArdapio.appendChild(img)
 
-    divC.appendChild(divItemCardapio)
+    divC.appendChild(divItemCArdapio)
 }
 
-//executando a função
-criarItemCardapio(
-    'Bolo de Chocolate', 
-    'Um clássico irresistível com camadas de chocolate', 
-    'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
-)
+
+function buscarBolos() {
+    const url = 'https://confeitaria-api-m796.onrender.com'; // Substitua com a URL real
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição: ' + response.statusText);
+            }
+            return response.json(); // Converte a resposta para JSON
+        })
+        .then(dados => {
+            console.log('Lista de bolos:', dados); // Exibe os dados no console
+            criarItemCardapio(dados.titulo, dados.descricao, dados.foto)
+        })
+        .catch(error => {
+            console.error('Erro ao fazer a requisição:', error); // Trata erros
+        });
+}
+
+// Chamada da função para buscar os bolos
+buscarBolos();
